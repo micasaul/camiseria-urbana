7fd2ea7fd6c9b9a5afc6231667a6aa05cc8ade37
Mic <mica020804@gmail.com>
2026-01-26 04:54:31 -0300

    Agregar header ngrok-skip-browser-warning para peticiones API

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -5,6 +5,19 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
+const originalFetch = window.fetch;
+window.fetch = function(url, config = {}) {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  if (backendUrl && url.toString().includes(backendUrl)) {
+    config.headers = {
+      ...config.headers, 
+      "ngrok-skip-browser-warning": "true", 
+    };
+  }
+  return originalFetch(url, config);
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
