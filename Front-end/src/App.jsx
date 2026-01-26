2211fb80f9aa1ba59d01e670b478dc7591ddb81b
Mic <mica020804@gmail.com>
2026-01-26 03:19:37 -0300

    Mostrar BACKEND_URL en consola para debug

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,11 +1,15 @@
-import AppRoutes from "./routes/AppRoutes.jsx"
-import './App.css'
+import { useEffect } from "react";
+import AppRoutes from "./routes/AppRoutes.jsx";
+import './App.css';
 
 function App() {
+  useEffect(() => {
+    console.log("BACKEND_URL runtime:", import.meta.env.BACKEND_URL);
+  }, []);
+
   return (
     <AppRoutes />
-  )
+  );
 }
 
-export default App
- 
+export default App;
